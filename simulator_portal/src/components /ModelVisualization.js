8c9649faebaa7e8b8f6356624f5e0b87f6f2d37e
Matheus  Esteves Zanoto <CIANDT\matheus.zanoto@lnb035003cps.ciandt.global>
2024-11-21 23:05:06 -0300

    feat: container para exibicao das metricas de avaliacao do modelo

--- simulator_portal/src/components /ModelVisualization.js
+++ simulator_portal/src/components /ModelVisualization.js
@@ -1,8 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
+import "./ModelVisualization.css";
 
 function ModelVisualization({ html }) {
     return (
-        <iframe srcDoc={html} sandbox="allow-scripts" width="100%" height="1000vh" />
+        <div class="model-visualization-area">
+            <iframe srcDoc={html} sandbox="allow-scripts" width="100%" height="1000vh" />
+            <div class="model-visualization-metrics">
+                <div className="model-visualization-metrics-label">
+                    MÉTRICAS DE AVALIAÇÃO (TO DO)
+                </div>
+            </div>
+        </div>
     )
 }
 
